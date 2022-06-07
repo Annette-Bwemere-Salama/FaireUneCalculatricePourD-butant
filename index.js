478a54e8bb746bee715fe4788d1d09ff2cfdba81
annette BWEMERE <bwemereannette@gmail.com>
2022-06-07 08:05:02 +0200

    style: ajout du style et arrangement titre

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log('hello_Annette Bwemere');
+//console.log('hello_Annette Bwemere');
+const touches = [... document.querySelectorAll('.bouton')];
+const listeKeycode = touches.map(touches => touches.dataset.key);
+console.log(listeKeycode);
