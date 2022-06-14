b7db1390a30fabb70b6b80bfa6ebacd1967216ce
annette BWEMERE <bwemereannette@gmail.com>
2022-06-14 14:32:37 +0200

    style : ajout du style

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
-//console.log('hello_Annette Bwemere');
+console.log('hello_Annette Bwemere');
 const touches = [... document.querySelectorAll('.bouton')];
-const listeKeycode = touches.map(touches => touches.dataset.key);
-console.log(listeKeycode);
+const listeKeycode = touches.map(touches => touche.dataset.key);
+//Touche 6 : keycode 102
+
+document.addEventListener('keydown',(e) => {
+  const valeur = e.keycode.toString();
+  calculer(valeur)
+  
+})
+
+document.addEventListener('click',(e) => {
+    const valeur = e.target.dataset.key;
+    calculer(valeur)
+})
+
+const calculer = (valeur) =>{
+    if(listeKeycode.includes(valeur)){
+        switch (valeur){
+            case '8':
+        ecran,textContent = "";
+             break
+            case '13':
+                const calcul = aval(ecran.textContent);
+                ecran.textContent = calcul;
+
+        }
+    }
+}
+
+
+
+
+
