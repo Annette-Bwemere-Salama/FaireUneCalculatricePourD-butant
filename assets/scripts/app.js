3c7c6663a92d6a078201627b22c39166628fc38b
Annette Bwemere <bwemereannette@gmail.com>
2022-12-29 13:01:37 +0200

    feat: merge

---
+++ assets/scripts/app.js
@@ -0,0 +1,10 @@
+
+
+let myResult = 0;
+
+function addNumbers(number1, number2){
+   myResult = number1 + number2;
+   outputResult(myResult, '');
+}
+
+addBtn.addEventListener("click", addNumbers);
