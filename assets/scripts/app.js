6f1cd084c1ce55c5700e6deb9ec55d8b6af26bad
Annette Bwemere <bwemereannette@gmail.com>
2023-01-02 02:21:01 +0200

    feat: addfunction

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,10 +1,25 @@
+const defaultResult = 0;
+const currentResult = defaultResult;
 
 
-let myResult = 0;
+// let myResult = 0;
 
-function addNumbers(number1, number2){
-   myResult = number1 + number2;
-   outputResult(myResult, '');
+// function addNumbers(number1, number2){
+//    myResult = number1 + number2;
+//    outputResult(myResult, '');
+// }
+
+// addBtn.addEventListener("click", addNumbers);
+
+function add(num1,num2) {
+   const result = num1 + num2;
+   alert(`The result of addition is ${result}`)
 }
+add(1,3);
+add(2,4)
 
-addBtn.addEventListener("click", addNumbers);
+currentResult = (currentResult + 10)*3/2-1;
+
+let calculationDescription = `(${defaultResult}+10)*3/2-1`
+
+outputResult(currentResult, calculationDescription);
