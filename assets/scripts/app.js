c0ced1e59f06f19f21a40547679067128b5819d3
Annette Bwemere <bwemereannette@gmail.com>
2023-01-03 10:15:40 +0200

    feat: defaultResult

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,11 @@
 console.log("Anny");
+const defaultResult = 0;
 
-let currentResult = 0;
+let currentResult = defaultResult;
 
 currentResult = (currentResult + 10)*3/2-1;
 
-outputResult(currentResult, '')
+let calculationDescription = '('+ defaultResult + '+10)*3/2-1'
+
+outputResult(currentResult, calculationDescription);
+
