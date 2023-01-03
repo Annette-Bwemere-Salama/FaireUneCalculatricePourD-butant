a6a618833807c836686a84da89aa3c79f92d54b2
Annette Bwemere <bwemereannette@gmail.com>
2023-01-03 09:56:36 +0200

    feat: début

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,25 +1,7 @@
-const defaultResult = 0;
-const currentResult = defaultResult;
+console.log("Anny");
 
-
-// let myResult = 0;
-
-// function addNumbers(number1, number2){
-//    myResult = number1 + number2;
-//    outputResult(myResult, '');
-// }
-
-// addBtn.addEventListener("click", addNumbers);
-
-function add(num1,num2) {
-   const result = num1 + num2;
-   alert(`The result of addition is ${result}`)
-}
-add(1,3);
-add(2,4)
+let currentResult = 0;
 
 currentResult = (currentResult + 10)*3/2-1;
 
-let calculationDescription = `(${defaultResult}+10)*3/2-1`
-
-outputResult(currentResult, calculationDescription);
+outputResult(currentResult, '')
