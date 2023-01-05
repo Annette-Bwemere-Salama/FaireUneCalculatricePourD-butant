d101751343131195d0da3694b6578440c91e646a
Annette Bwemere <bwemereannette@gmail.com>
2023-01-05 12:00:32 +0200

    feat; executing function indirecty

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,16 +3,18 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 
-function add(num1, num2) {
-    let result = num1+num2
-    return result
+function add() {
+    currentResult = currentResult + userInput.value;
+
 }
 
-currentResult = add(1, 2)
 
 
-let calculationDescription = `( ${defaultResult} + 10) * 3 / 2 - 1`;
+// let calculationDescription = `( ${defaultResult} + 10) * 3 / 2 - 1`;
 
+// add()
+
+addBtn.addEventListener('click', add)
 outputResult(currentResult, calculationDescription);
 
 
