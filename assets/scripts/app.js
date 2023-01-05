944bdc93ec4471610b80c63f5a0f688ad908dfea
Annette Bwemere <bwemereannette@gmail.com>
2023-01-05 12:07:08 +0200

    feat: parseInt string to number

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,18 +4,12 @@ let currentResult = defaultResult;
 
 
 function add() {
-    currentResult = currentResult + userInput.value;
-
+    currentResult = currentResult + parseInt(userInput.value);
+    outputResult(currentResult, '');
 }
 
 
-
-// let calculationDescription = `( ${defaultResult} + 10) * 3 / 2 - 1`;
-
-// add()
-
 addBtn.addEventListener('click', add)
-outputResult(currentResult, calculationDescription);
 
 
 
