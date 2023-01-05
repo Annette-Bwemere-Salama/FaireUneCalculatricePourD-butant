553accf71489fbcc1fc308f0fa8431996895d5bd
Annette Bwemere <bwemereannette@gmail.com>
2023-01-05 11:47:38 +0200

    feat: global variable and local

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,19 +1,20 @@
-console.log("Anny");
 const defaultResult = 0;
 
 let currentResult = defaultResult;
+
 
 function add(num1, num2) {
     let result = num1+num2
     return result
 }
 
-const additionResult = add(1, 2)
+currentResult = add(1, 2)
 
-currentResult = additionResult;
 
 let calculationDescription = `( ${defaultResult} + 10) * 3 / 2 - 1`;
 
 outputResult(currentResult, calculationDescription);
+
+
 
 
